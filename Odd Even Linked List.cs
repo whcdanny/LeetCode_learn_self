dcd758e07580705df787f34a845944a6fd4a589b
whcdanny <whcfoxdanny@gmail.com>
2020-05-23 22:10:11 -0400

    328.Odd Even Linked List

---
+++ Odd Even Linked List.cs
@@ -0,0 +1,46 @@
+/*Given a singly linked list, group all odd nodes together followed by the even nodes. Please note here we are talking about the node number and not the value in the nodes.
+
+You should try to do it in place. The program should run in O(1) space complexity and O(nodes) time complexity.
+
+Example 1:
+
+Input: 1->2->3->4->5->NULL
+Output: 1->3->5->2->4->NULL
+Example 2:
+
+Input: 2->1->3->5->6->4->7->NULL
+Output: 2->3->6->7->1->5->4->NULL*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+ /*思路：把odd取出 用even和even.next 来决定是否继续*/
+public class Solution {
+    public ListNode OddEvenList(ListNode head) {        
+        if (head == null || head.next == null)
+                return head;
+        ListNode odd = head;
+        ListNode even = new ListNode();
+        if(head.next != null)
+            even = head.next;
+        else
+            even = null;
+        ListNode res = even;
+        while (even != null && even.next != null)
+        {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = res;
+        return head;
+    }
+}
