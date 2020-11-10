c0fd955d70115e1da466a1337adfbe983359e76b
whcdanny <whcfoxdanny@gmail.com>
2020-11-10 14:17:54 -0500

    79. Word Search

---
+++ 79. Word Search.cs
@@ -0,0 +1,33 @@
+public class Solution {
+    public bool Exist(char[][] board, string word) {
+        if (board.Length == 0) 
+            return false;
+        int rows = board.Length;
+        int cols = board[0].Length;
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                if (isExist(board, i, j, rows-1, cols-1, word, 0)) 
+                    return true;
+            }
+        }
+
+        return false;
+    }
+    public bool isExist(char[][] board, int i, int j, int rows, int cols, string word, int wordIndex)
+    {
+        if (wordIndex == word.Length) 
+            return true;
+        if (i < 0 || j < 0 || i > rows || j > cols || board[i][j] != word[wordIndex]) 
+            return false;
+
+        board[i][j] = ' ';
+        bool check = isExist(board, i - 1, j, rows, cols, word, wordIndex + 1) ||
+                    isExist(board, i, j - 1, rows, cols, word, wordIndex + 1) ||
+                    isExist(board, i, j + 1, rows, cols, word, wordIndex + 1) ||
+                    isExist(board, i + 1, j, rows, cols, word, wordIndex + 1);
+        board[i][j] = word[wordIndex];
+        return check;
+    }
+}
