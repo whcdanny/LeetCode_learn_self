e39ccf0f37787d5ff8b5a6d022880d7369634f86
Haochen_Wang <whcfoxdanny@gmail.com>
2021-01-03 15:09:03 -0500

    111. Minimum Depth of Binary Tree

---
+++ 111. Minimum Depth of Binary Tree.cs
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int MinDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        if (root.left == null)
+            return MinDepth(root.right) + 1;
+        if (root.right == null)
+            return MinDepth(root.left) + 1;
+        return Math.Min(MinDepth(root.left), MinDepth(root.right))+1;
+    }
+}
+
+//Java
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int minDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        if(root.left==null){
+            return minDepth(root.right)+1;
+        }
+        if(root.right==null){
+            return minDepth(root.left)+1;
+        }
+        return Math.min(minDepth(root.left), minDepth(root.right))+1;
+    }
+}
