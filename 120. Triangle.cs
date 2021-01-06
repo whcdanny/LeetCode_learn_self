f9ecc104aa3c4eda0368c0e95f4c688bc705e872
Haochen_Wang <whcfoxdanny@gmail.com>
2021-01-05 21:05:36 -0500

    120. Triangle

---
+++ 120. Triangle.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int MinimumTotal(IList<IList<int>> triangle) {
+        if (triangle == null || triangle.Count == 0)
+            return 0;
+        
+        for (int i = triangle.Count - 1; i > 0; i--)
+            for (int j = 0; j < triangle[i].Count - 1; j++)
+                triangle[i - 1][j] += Math.Min(triangle[i][j], triangle[i][j + 1]);
+        
+        return triangle[0][0];
+    }
+}
