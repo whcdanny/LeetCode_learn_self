d0bbff0ed6bbd3686e9cb20aae347a624ef0203a
Haochen_Wang <whcfoxdanny@gmail.com>
2021-01-13 17:32:17 -0500

    137. Single Number II

---
+++ 137. Single Number II.cs
@@ -0,0 +1,27 @@
+public class Solution {
+    public int SingleNumber(int[] nums) {
+        if (nums == null)
+                return 0;
+            if (nums.Length == 1)
+                return nums[0];
+            Array.Sort(nums);
+            int res = nums[0];
+            int time = 0;
+            for(int i=1; i<nums.Length; i++)
+            {
+                if (res == nums[i])
+                    time += 1;
+                if(res != nums[i])
+                {
+                    if (time == 2)
+                    {
+                        res = nums[i];
+                        time = 0;
+                    }                        
+                    else
+                        break;
+                }
+            }
+            return res;
+    }
+}
