e4b3a6ead151e66860e315ee7849d3198fb05b3f
Haochen_Wang <whcfoxdanny@gmail.com>
2021-01-28 11:53:10 -0500

    177. Nth Highest Salary

---
+++ 177. Nth Highest Salary.sql
@@ -0,0 +1,10 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+      # Write your MySQL query statement below.
+      SELECT MAX(Salary) FROM Employee E1
+      WHERE N - 1 =
+      (SELECT COUNT(DISTINCT(E2.Salary)) FROM Employee E2
+      WHERE E2.Salary > E1.Salary)
+  );
+END
