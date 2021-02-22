2fb711672deb2165695d4ae3ff5b0fdbf9ac09f1
Haochen_Wang <whcfoxdanny@gmail.com>
2021-02-22 13:28:55 -0500

    222. Count Complete Tree Nodes

---
+++ 222. Count Complete Tree Nodes.cs
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public int CountNodes(TreeNode root) {
+        if(root == null) 
+            return 0; 
+        return 1 + CountNodes(root.left) + CountNodes(root.right);
+    }
+}
+
+//Java
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        if(root == null) 
+            return 0; 
+         return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+}
