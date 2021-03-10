54f07fa096cbea2375239df8da222c3fe25a5e7c
Haochen_Wang <whcfoxdanny@gmail.com>
2021-03-10 08:05:50 -0500

    updateC++

--- 260. Single Number III.cs
+++ 260. Single Number III.cs
@@ -25,3 +25,21 @@ public class Solution {
         return res.ToArray();
     }
 }
+
+//C++
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        set <int> s;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (s.count(nums[i]) > 0)
+                s.erase(nums[i]);
+            else
+                s.insert(nums[i]);
+        }
+        vector <int> res;
+        copy(s.begin(), s.end(), back_inserter(res));
+        return res;
+    }
+};
